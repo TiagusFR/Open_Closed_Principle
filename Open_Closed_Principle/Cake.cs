93294535df383742e847030db60181ff3c3fb468
Tiago França <t.iago_196@hotmail.com>
2024-07-15 14:35:46 -0300

    Adicionar arquivos de projeto.

---
+++ Open_Closed_Principle/Cake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle
+{
+    internal class Cake : IRecepie
+    {
+        public string Ingredients { get; set; }
+        public string Baking { get; set; }
+
+        public string GetIngredients() 
+        {
+            return Ingredients + Baking;
+        }
+    }
+}
---
+++ Open_Closed_Principle/Rice.cs
@@ -0,0 +1,13 @@
+﻿namespace Open_Closed_Principle
+{
+    internal class Rice : IRecepie
+    {
+        public string Ingredients { get; set; }
+        public string Boiling { get; set; }
+
+        public string GetIngredients ()
+        {
+            return Ingredients + Boiling;
+        }
+    }
+}
